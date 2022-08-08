2c3f79676138bcd3cbe6f28b568bb3ac2400a484
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-08-08 14:15:57 +0600

    jwt setup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -10,14 +11,34 @@ const ObjectId = require('mongodb').ObjectId;
 app.use(cors())
 app.use(express.json());
 
+//verify
+
+
 // connect mongo db
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qil1a.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unAuthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 async function run() {
     try {
         await client.connect();
         const moviesCollection = client.db("goldmines").collection("movies");
+        const userCollection = client.db("goldmines").collection("users");
+
         console.log('db connected');
 
         app.get('/movies', async (req, res) => {
@@ -26,6 +47,18 @@ async function run() {
             const users = await cursor.toArray();
             res.send(users);
         });
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        })
 
         app.get('/search/', async (req, res) => {
             const search = req.query.search
@@ -43,12 +76,19 @@ async function run() {
         });
 
 
-        app.get('/movies/:id', async (req, res) => {
+        app.get('/movies/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await moviesCollection.findOne(query);
             res.send(result);
         });
+
+        app.delete('/movies/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await moviesCollection.deleteOne(filter);
+            res.send(result);
+        })
     }
     finally {
 
