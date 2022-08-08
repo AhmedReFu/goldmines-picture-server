1891c393d636c3666600e175d621325c48136918
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-08-09 03:25:20 +0600

    admin user setup

--- index.js
+++ index.js
@@ -47,6 +47,35 @@ async function run() {
             const users = await cursor.toArray();
             res.send(users);
         });
+        app.get('/popular', async (req, res) => {
+            const query = { Popular: 'Yes' };
+            const cursor = moviesCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+        app.get('/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        });
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            } else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
+        })
+
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
@@ -89,6 +118,10 @@ async function run() {
             const result = await moviesCollection.deleteOne(filter);
             res.send(result);
         })
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
     }
     finally {
 
